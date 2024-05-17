e56c944689a6cb9382b2ab07e693b77d9b58642b
Ingryd Barbosa <araujoingryd81@gmail.com>
2024-05-17 18:49:40 -0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('themeToggleButton');
+    const body = document.body;
+
+    themeToggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            themeToggleButton.textContent = 'Alternar para Tema Claro';
+        } else {
+            themeToggleButton.textContent = 'Alternar para Tema Escuro';
+        }
+    });
+});
+
 
